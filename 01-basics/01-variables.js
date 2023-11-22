6015d1d7c4ade11d8b3d137aa0b304b7897ce74c
UzmaInayat <149899219+UzmaInayat@users.noreply.github.com>
2023-11-22 18:32:35 +0000

    config done

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -2,3 +2,7 @@ const accountId = 144553
 let accountEmail = "uzma@gmail.com"
 var accountPassword = "123345"
 accountCity = "Hazro"
+
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword,accountCity ]);
---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,14 @@
+"Use strict";
+
+// alert( 3+3);
+
+// console.log(12334);
+
+let name = "uzmia"
+let age = 25
+let isLoggedIn = false
+
+// console.log(typeof "12334");
+
+// console.log(typeof "name");
+console.log(typeof null);
